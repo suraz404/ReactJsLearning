12f4ff971a8a071caabf10bf0c56711abff482b6
Suraz Agrahari <surazagrahari1980@gmail.com>
2025-12-09 23:15:00 +0545

    to do list project added

--- ReactJsLearning/src/App.jsx
+++ ReactJsLearning/src/App.jsx
@@ -1,4 +1,4 @@
-import { ToggleSwitch } from "./ToggleSwitch Project/ToggleSwitch"
+import { Todo } from "./TodoList/Todo"  
 
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <ToggleSwitch/>
+      <Todo/>
     </>
   )
 }
